2fcf3b513d9bb6f6cecb97fd2d86c18ccf1b438e
Fihaa <haikalwatampone3@gmail.com>
2022-07-07 19:21:46 +0800

    Fix Model

--- src/models/user.go
+++ src/models/user.go
@@ -9,11 +9,11 @@ import (
 type User struct {
 	ID        uint64         `gorm:"AUTO_INCREMENT" json:"id"`
 	Name      string         `json:"name" binding:"required" gorm:"type:varchar(50)"`
-	Username  string         `gorm:"unique;type:varchar(50)" json:"username" binding:"required"`
-	Email     string         `gorm:"unique;type:varchar(50)" json:"email" binding:"required,email"`
-	Password  string         `json:"password" binding:"required" gorm:"type:varchar(255)"`
-	Role      string         `json:"role" binding:"required"`
-	Avatar    string         `json:"avatar"`
+	Username  string         `gorm:"unique;type:varchar(50);NOT NULL" json:"username" binding:"required"`
+	Email     string         `gorm:"unique;type:varchar(50);NOT NULL" json:"email" binding:"required,email"`
+	Password  string         `json:"password" binding:"required" gorm:"type:varchar(255);NOT NULL"`
+	Role      string         `json:"role" binding:"required" gorm:"type:varchar(10);NOT NULL"`
+	Avatar    string         `json:"avatar" gorm:"type:varchar(50)" `
 	Token     string         `gorm:"type:varchar(255)" json:"token,omitempty"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
